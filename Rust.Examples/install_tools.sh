d48d74fb44a6c6872cd55eb0e9958cda2a3144a0
Tony Yousefnezhad <myousefnezhad@gmail.com>
2026-05-31 15:19:52 -0600

    Rust toosl

---
+++ Rust.Examples/install_tools.sh
@@ -0,0 +1,9 @@
+rustup component add rust-analyzer
+rustup component add rustfmt
+rustup component add clippy
+cargo install cargo-watch # Rebuild on file changes
+cargo install --locked cargo-nextest # Faster test runner
+cargo install cargo-expand # Expand proc macros
+cargo install cargo-audit # Security vulnerability scan
+cargo install cargo-deny # License/dependency validation
+cargo install bacon # Fast terminal dashboard
