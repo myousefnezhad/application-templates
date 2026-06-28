57df53584138cfa134dc33a7b557b465d029e74c
Tony Yousefnezhad <myousefnezhad@gmail.com>
2026-06-28 12:49:03 -0600

    QWEN: updating download list

--- Python.Examples/ModelSurgery/QWEN_3.6_or_3.5_35B-A3B_or_27B/PyTorch/download.sh
+++ Python.Examples/ModelSurgery/QWEN_3.6_or_3.5_35B-A3B_or_27B/PyTorch/download.sh
@@ -1,1 +1,4 @@
-hf download Qwen/Qwen3.5-35B-A3B --local-dir ../Qwen3.5-35B-A3B
+hf download Qwen/Qwen3.6-35B-A3B --local-dir ../Qwen3.6-35B-A3B
+#hf download Qwen/Qwen3.5-35B-A3B --local-dir ../Qwen3.5-35B-A3B
+#hf download Qwen/Qwen3.6-27B --local-dir ../Qwen3.6-27B
+#hf download Qwen/Qwen3.5-27B --local-dir ../Qwen3.5-27B
